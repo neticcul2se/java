cdc05c098868b4098a84a68ac6c9bdde22013a0c
Pae <cul2se@gmail.com>
2015-04-17 18:38:47 +0700

    thread

---
+++ src/test/Test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Pae
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+Thread t = Thread.currentThread();
+    System.out.println(t.getName());
+    
+            System.out.println(t.getPriority());
+            System.out.println(t.isAlive());
+                System.out.println(t.isDaemon());
+    }
+    
+}
